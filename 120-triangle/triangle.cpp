203ffe3c44c7d8839029c27da6e27337d197455f
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-02 17:01:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (8.74%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -18,19 +18,22 @@ class Solution
         int n = triangle.size();
         vector<vector < int>> dp(n, vector<int> (n, -1e9));
        	// return solve(0,0,n,triangle,dp);
+        vector<int>temp(n,0);
         for (int j = 0; j < n; j++)
         {
-            dp[n - 1][j] = triangle[n - 1][j];
+            temp[j] = triangle[n - 1][j];
         }
         for (int i = n - 2; i >= 0; i--)
         {
+            vector<int>curr(n,0);
             for (int j = i; j >= 0; j--)
             {
-                int down = triangle[i][j] + dp[i + 1][j];
-                int diag = triangle[i][j] + dp[i + 1][j + 1];
-                dp[i][j] = min(down, diag);
+                int down = triangle[i][j] + temp[j];
+                int diag = triangle[i][j] + temp[j + 1];
+                curr[j] = min(down, diag);
             }
+            temp=curr;
         }
-        return dp[0][0];
+        return temp[0];
     }
 };

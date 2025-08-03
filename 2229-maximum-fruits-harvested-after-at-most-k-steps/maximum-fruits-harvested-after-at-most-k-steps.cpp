8aa85415ab46fc11a2b6262b7b05a63bd10626a2
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-04 00:18:56 +0530

    Time: 99 ms (28.51%) | Memory: 128.4 MB (53.19%) - LeetSync

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
-        int n = fruits.size();
-        vector<int> pos(n), psum(n);
-        for (int i = 0; i < n; ++i)
-            pos[i] = fruits[i][0];
-        
-        psum[0] = fruits[0][1];
-        for (int i = 1; i < n; ++i)
-            psum[i] = psum[i - 1] + fruits[i][1];
-
-        auto getSum = [&](int l, int r) -> int {
-            if (l > r) return 0;
-            return psum[r] - (l > 0 ? psum[l - 1] : 0);
-        };
-
-        int maxFruits = 0;
-
-        for (int d = 0; d <= k/2; ++d) {
-            int left = startPos - d;
-            int right = startPos + max(0, k - 2 * d);
-            auto itL = lower_bound(pos.begin(), pos.end(), left);
-            auto itR = upper_bound(pos.begin(), pos.end(), right);
-            int i = itL - pos.begin();
-            int j = itR - pos.begin() - 1;
-            // if (i <= j && i < n && j < n)
-            maxFruits = max(maxFruits, getSum(i, j));
+    int maxTotalFruits(vector<vector<int>>& fruits, int p, int k) {
+        int n=fruits.size();
+        vector<int>v(n,0);
+        vector<int>psum(n,0);
+        for(int i=0;i<n;i++){
+            v[i]=fruits[i][0];
         }
-
-        for (int d = 0; d <= k/2; ++d) {
-            int right = startPos + d;
-            int left = startPos - max(0, k - 2 * d);
-            auto itL = lower_bound(pos.begin(), pos.end(), left);
-            auto itR = upper_bound(pos.begin(), pos.end(), right);
-            int i = itL - pos.begin();
-            int j = itR - pos.begin() - 1;
-            // if (i <= j && i < n && j < n)
-            maxFruits = max(maxFruits, getSum(i, j));
+        psum[0]=fruits[0][1];
+        for(int i=1;i<n;i++){
+            psum[i]=fruits[i][1]+psum[i-1];
         }
-
-        return maxFruits;
+        int maxi=0;
+        for(int d=0;d<=k/2;d++){
+            int i=p-d;
+            int j=p+max(0,(k-2*d));
+            auto it=lower_bound(v.begin(),v.end(),i);
+            int left=it-v.begin();
+            auto it2=upper_bound(v.begin(),v.end(),j)-1;
+            int right=it2-v.begin();
+            if (left <= right && left < n && right < n)
+            maxi=max(maxi,psum[right] - (left > 0 ? psum[left - 1] : 0));
+            i=p-max(0,(k-2*d));
+            j=p+d;
+            it=lower_bound(v.begin(),v.end(),i);
+            left=it-v.begin();
+            it2=upper_bound(v.begin(),v.end(),j)-1;
+            right=it2-v.begin();
+            if (left <= right && left < n && right < n)
+            maxi=max(maxi,psum[right] - (left > 0 ? psum[left - 1] : 0));
+        }
+        return maxi;
     }
 };

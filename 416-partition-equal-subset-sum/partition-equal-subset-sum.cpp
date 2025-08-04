1e2924f25e6337416572a933cb48e57790a15bd2
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-05 00:46:46 +0530

    Time: 163 ms (26.65%) | Memory: 18.9 MB (53.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    bool solve(int ind,int target,vector<int>& a,vector<vector<int>>&dp){
-        if(target==0)return true;
-        if(ind==0)return a[ind]==target;
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        bool nt=solve(ind-1,target,a,dp);
-        bool t=false;
-        if(target>=a[ind]){
-            t=solve(ind-1,target-a[ind],a,dp);
+    bool solve(int ind, int target, vector<int>& a, vector<vector<int>>& dp) {
+        if (target == 0)
+            return true;
+        if (ind == 0)
+            return a[ind] == target;
+        if (dp[ind][target] != -1)
+            return dp[ind][target];
+        bool nt = solve(ind - 1, target, a, dp);
+        bool t = false;
+        if (target >= a[ind]) {
+            t = solve(ind - 1, target - a[ind], a, dp);
         }
-        return dp[ind][target]= t||nt;
+        return dp[ind][target] = t || nt;
     }
     bool canPartition(vector<int>& a) {
-        int sum=accumulate(a.begin(),a.end(),0);
-        int n=a.size();
-        int target=sum/2;
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        if(sum%2!=0)return false;
-        else{
-            return solve(n-1,target,a,dp);
+        int sum = accumulate(a.begin(), a.end(), 0);
+        int n = a.size();
+        int target = sum / 2;
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, 0));
+        if (sum % 2 != 0)
+            return false;
+        else {
+            for (int i = 0; i < n; i++) {
+                dp[i][0] = true;
+            }
+            if (a[0] <= target)
+            dp[0][a[0]] = true;
+            
+            for (int ind = 1; ind < n; ind++) {
+                for (int j = 1; j <= target; j++) {
+                    bool nt = dp[ind - 1][j];
+                    bool t = false;
+                    if (j >= a[ind]) {
+                        t = dp[ind - 1][j - a[ind]];
+                    }
+                    dp[ind][j] = t || nt;
+                }
+            }
+            return dp[n - 1][target];
         }
-        
     }
 };

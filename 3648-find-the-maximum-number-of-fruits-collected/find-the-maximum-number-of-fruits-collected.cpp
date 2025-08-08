267a8331ebd0670b58f5b729713ed9023eaf5edc
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-08 12:18:51 +0530

    Time: 209 ms (25.92%) | Memory: 157.1 MB (50.62%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -1,70 +1,40 @@
 class Solution {
-int n;
-vector<vector<int>> t;
 public:
-    int child1Collect(vector<vector<int>>& grid) {
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans += grid[i][i];
-            grid[i][i] = 0;
-            t[i][i] = 0;
-        }
-        return ans;
-    }
+int c1path(int i, int j,int n, vector<vector<int>>& a, vector<vector<int>>& dp) {
+    if (i < 0 || i >= n || j < 0 || j >= n || i>=j) return -1e9;
+    if (i == 0 && j == n-1) return a[i][j];
+    if (dp[i][j] != -1) return dp[i][j];
 
-    int child2Collect(int i, int j, vector<vector<int>>& grid) {
-        if (i < 0 || i >= n || j < 0 || j >= n) {
-            return 0;
+    int rside = a[i][j] + c1path(i - 1, j + 1, n, a, dp);
+    int uppar = a[i][j] + c1path(i - 1, j,  n, a, dp);
+    int lside=a[i][j] + c1path(i - 1, j - 1, n, a, dp);
+
+    return dp[i][j] = max({rside, uppar, lside});
+}
+
+
+   int c2path(int i, int j,int n, vector<vector<int>>& a, vector<vector<int>>& dp) {
+    if (i < 0 || i >= n || j < 0 || j >= n || j>=i) return -1e9;
+    if (i == n-1 && j == 0) return a[i][j];
+    if (dp[i][j] != -1) return dp[i][j];
+
+    int side = a[i][j] + c2path(i, j - 1, n, a, dp);
+    int uppar = a[i][j] + c2path(i - 1, j - 1, n, a, dp);
+    int neeche=a[i][j] + c2path(i + 1, j - 1, n, a, dp);
+
+    return dp[i][j] = max({side, uppar, neeche});
+}
+int maxCollectedFruits(vector<vector<int>>& a) {
+        int n=a.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i][i];
         }
-        if (i == n - 1 && j == n - 1) {
-            return 0;
-        }
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+
+        sum+=c2path(n-1,n-2,n,a,dp);
+        sum+=c1path(n-2,n-1,n,a,dp);
+        return sum;
         
-        if (i == j || i > j) {
-            return 0;
-        }
-
-        if (t[i][j] != -1)
-            return t[i][j];
-
-        int leftcorner = grid[i][j] + child2Collect(i + 1, j - 1, grid);
-        int middle = grid[i][j] + child2Collect(i + 1, j, grid);
-        int rightcorner = grid[i][j] + child2Collect(i + 1, j + 1, grid);
-
-        return t[i][j] = max({middle, rightcorner, leftcorner});
     }
-
-    int child3Collect(int i, int j, vector<vector<int>>& grid) {
-        if (i < 0 || i >= n || j < 0 || j >= n) {
-            return 0;
-        }
-        if (i == n - 1 && j == n - 1) {
-            return 0;
-        }
-
-        if (i == j || j > i) {
-            return 0;
-        }
-        if (t[i][j] != -1)
-            return t[i][j];
-
-        int topcorner   = grid[i][j] + child3Collect(i - 1, j + 1, grid);
-        int right       = grid[i][j] + child3Collect(i, j + 1, grid);
-        int rightcorner = grid[i][j] + child3Collect(i + 1, j + 1, grid);
-
-        return t[i][j] = max({right, rightcorner, topcorner});
-    }
-
-    int maxCollectedFruits(vector<vector<int>>& grid) {
-        n = grid.size();
-        t.resize(n, vector<int>(n, -1));
-
-        int firstChildScore = child1Collect(grid);
-
-        int secondChildScore = child2Collect(0, n - 1, grid);
-
-        int thirdChildScore  = child3Collect(n - 1, 0, grid);
-
-        return (firstChildScore + secondChildScore + thirdChildScore);
-    }    
 };

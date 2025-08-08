bdcfa2a531c5d5cb570903fd744822dc8d073730
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-08 19:46:10 +0530

    Time: 6 ms (69.83%) | Memory: 13.2 MB (61.67%) - LeetSync

--- 826-soup-servings/soup-servings.cpp
+++ 826-soup-servings/soup-servings.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    double solve(int a,int b,int n,vector<vector<double>>&dp){
+    unordered_map<int, unordered_map<int, double>> dp;
+    double solve(int a,int b,int n){
         if(a>=n && b>=n)return 0.5;
         if(a>=n && b<n)return 1;
         if(a<n && b>=n)return 0;
-        if(dp[a][b]!=-1)return dp[a][b];
-        double o1=solve(a+100,b+0,n,dp);
-        double o2=solve(a+75,b+25,n,dp);
-        double o3=solve(a+50,b+50,n,dp);
-        double o4=solve(a+25,b+75,n,dp);
+        if(dp[a][b]!=0)return dp[a][b];
+        double o1=solve(a+100,b+0,n);
+        double o2=solve(a+75,b+25,n);
+        double o3=solve(a+50,b+50,n);
+        double o4=solve(a+25,b+75,n);
 
         return dp[a][b]=0.25*(o1+o2+o3+o4);
     }
     double soupServings(int n) {
         if (n >= 5000) return 1.0;
-        vector<vector<double>>dp(5000,vector<double>(5000,-1));
-        return solve(0,0,n,dp);
+        return solve(0,0,n);
     }
 };

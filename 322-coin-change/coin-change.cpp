e74a2c59dd5d4fce2741a01e8467a4a11e6458bf
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-09 01:37:27 +0530

    Time: 49 ms (29.52%) | Memory: 49.5 MB (26.97%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -8,7 +8,7 @@ public:
             if(target%c[i]==0){
                 return target/c[i];
             }
-            return 1e9;
+            else return 1e9;
         }
         if(dp[i][target]!=-1)return dp[i][target];
         int nt=0+solve(i-1,target,c,dp);
@@ -20,26 +20,26 @@ public:
     }
     int coinChange(vector<int>& c, int amount) {
         int n=c.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-        // int ans=solve(n-1,amount,c,dp);
-        // if(ans==1e9)return -1;
-        // return ans;
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans=solve(n-1,amount,c,dp);
+        if(ans==1e9)return -1;
+        return ans;
 
-        for(int i=0;i<=amount;i++){
-            if(i%c[0]==0){
-                dp[0][i]=i/c[0];
-            }else dp[0][i]=1e9;
-        }
-        for(int i=1;i<n;i++){
-            for(int target=0;target<=amount;target++){
-                int nt=0+dp[i-1][target];
-                int t=1e9;
-                if(target>=c[i])
-                t=1+dp[i][target-c[i]];
-                dp[i][target]=min(nt,t);
-            }
-        }
-        if(dp[n-1][amount]==1e9)return -1;
-        return dp[n-1][amount];
+        // for(int i=0;i<=amount;i++){
+        //     if(i%c[0]==0){
+        //         dp[0][i]=i/c[0];
+        //     }else dp[0][i]=1e9;
+        // }
+        // for(int i=1;i<n;i++){
+        //     for(int target=0;target<=amount;target++){
+        //         int nt=0+dp[i-1][target];
+        //         int t=1e9;
+        //         if(target>=c[i])
+        //         t=1+dp[i][target-c[i]];
+        //         dp[i][target]=min(nt,t);
+        //     }
+        // }
+        // if(dp[n-1][amount]==1e9)return -1;
+        // return dp[n-1][amount];
     }
 };

001c6ddab1d7eb77af480356a160509182df1c24
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-08 15:50:02 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (45.34%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.cpp
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -1,28 +1,19 @@
-class Solution {
-public:
-    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        stack<int>st;
-        int r=0;
-        for(int i=0;i<pushed.size();i++){
-            if(pushed[i]==popped[r]){
-                r++;
-                while(!st.empty() && st.top()==popped[r]){
+class Solution
+{
+    public:
+        bool validateStackSequences(vector<int> &pushed, vector<int> &popped)
+        {
+            stack<int> st;
+            int r = 0;
+            for (int i = 0; i < pushed.size(); i++)
+            {
+                st.push(pushed[i]);
+                while (!st.empty() && st.top() == popped[r])
+                {
                     st.pop();
                     r++;
                 }
             }
-            else{
-                st.push(pushed[i]);
-            }
+            return st.empty();
         }
-        for(int i=r;i<popped.size();i++){
-            if(popped[i]!=st.top()){
-                return false;
-            }else{
-                st.pop();
-            }
-        }
-        return true;
-        
-    }
 };

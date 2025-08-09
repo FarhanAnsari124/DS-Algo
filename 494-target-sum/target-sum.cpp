5559331791e69c014c54b77f7c11bf9ba42becea
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-09 17:06:46 +0530

    Time: 7 ms (69.15%) | Memory: 15.3 MB (52.26%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -17,8 +17,33 @@ public:
         int minus=solve(i-1,t+a[i],a);
         return dp[i][t]= plus+minus;
     }
+     int countPartitions(vector<int>& arr, int d) {
+        int totsum=accumulate(arr.begin(),arr.end(),0);
+        int s2=(totsum-d)/2;
+        if((totsum-d)%2==1 || totsum-d<0 )return 0;
+        int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(s2+1,0));
+        // return solve(n-1,s2,arr,dp);
+        if(arr[0]==0)dp[0][0]=2;
+        else{
+            dp[0][0]=1;
+        }
+        if(arr[0]!=0 && arr[0]<=s2)dp[0][arr[0]]=1;
+        for(int ind=1;ind<n;ind++){
+            for(int sum=0;sum<=s2;sum++){
+                int nt=dp[ind-1][sum];
+                int t=0;
+                if(sum>=arr[ind]){
+                    t=dp[ind-1][sum-arr[ind]];
+                }
+                dp[ind][sum]=nt+t;
+            }
+        }
+        return dp[n-1][s2];
+     }
     int findTargetSumWays(vector<int>& a, int target) {
         int n=a.size();
-        return solve(n-1,target,a);
+        // return solve(n-1,target,a);
+        return countPartitions(a,target);
     }
 };

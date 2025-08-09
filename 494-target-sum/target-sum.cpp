3b94ad020d84184d55c32188e8dc30f5bd89ac4e
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-09 16:50:24 +0530

    Time: 900 ms (10.13%) | Memory: 52.6 MB (15.61%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    unordered_map<int,unordered_map<int,int>>dp;
     int solve(int i,int t,vector<int>& a){
         if(i==0){
             if(a[i]==0 && a[i]==abs(t)){
@@ -11,9 +12,10 @@ public:
 
             else return 0;
         }
+        if(dp[i][t]!=0)return dp[i][t];
         int plus=solve(i-1,t-a[i],a);
         int minus=solve(i-1,t+a[i],a);
-        return plus+minus;
+        return dp[i][t]= plus+minus;
     }
     int findTargetSumWays(vector<int>& a, int target) {
         int n=a.size();

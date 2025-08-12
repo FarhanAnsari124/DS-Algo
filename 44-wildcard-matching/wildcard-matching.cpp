b7571617e777809a6452d581fc96559e8a1aabf1
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-12 21:21:12 +0530

    Time: 35 ms (42.95%) | Memory: 31.3 MB (43.10%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,10 +1,10 @@
 class Solution {//S1=p S2=s
 public:
     bool solve(int i,int j,string &p,string &s,vector<vector<int>>&dp){
-        if(i<0 && j<0)return true;
-        if(i<0 && j>=0)return false;
-        if(j<0 && i>=0){
-            for(int k=0;k<=i;k++){
+        if(i==0 && j==0)return true;
+        if(i==0 && j>0)return false;
+        if(j==0 && i>0){
+            for(int k=0;k<i;k++){
                 if(p[k]!='*'){
                     return false;
                 }
@@ -12,10 +12,10 @@ public:
             return true;
         }
         if(dp[i][j]!=-1)return dp[i][j];
-        if(p[i]==s[j] || p[i]=='?'){
+        if(p[i-1]==s[j-1] || p[i-1]=='?'){
             return dp[i][j]=solve(i-1,j-1,p,s,dp);
         }
-        if(p[i]=='*'){
+        if(p[i-1]=='*'){
             return dp[i][j]= (solve(i-1,j,p,s,dp)||solve(i,j-1,p,s,dp));
         }
         return dp[i][j]=false;
@@ -23,8 +23,8 @@ public:
     bool isMatch(string s, string p) {
         int n=p.size();
         int m=s.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,p,s,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solve(n,m,p,s,dp);
         
     }
 };

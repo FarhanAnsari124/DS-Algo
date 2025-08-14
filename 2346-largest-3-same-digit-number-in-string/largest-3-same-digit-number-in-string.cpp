aa4b9ea0fe97c4dc5c96b3c57393fd0ab19b2648
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-14 19:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (52.45%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,23 +1,36 @@
+string operator*(const string &s, int times) {
+    string result;
+    result.reserve(s.size() * times);
+    for (int i = 0; i < times; i++) {
+        result += s;
+    }
+    return result;
+}
+
+string operator*(int times, const string &s) {
+    return s * times;
+}
+
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        int n=num.size();
-        int ans=-1;
-        int cnt=0;
-      for(int i=1;i<n;i++){
-        if(cnt==2)ans=max(ans,int(num[i-1]));
-        if(num[i-1]==num[i]){
-            cnt++;
-        }
-        else cnt=0;
-      }
-      if(cnt==2)ans=max(ans,int(num[n-1]));
-      if(ans==-1)return "";
-        string t="";
-        t+=ans;
-        t+=ans;
-        t+=ans;
+        int n = num.size();
+        char ans = -1;
+        int cnt = 0;
 
-    return t ;
+        for (int i = 1; i < n; i++) {
+            if (cnt == 2) ans = max(ans, num[i - 1]);
+            if (num[i - 1] == num[i]) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+        }
+
+        if (cnt == 2) ans = max(ans, num[n - 1]);
+        if (ans == -1) return "";
+
+        string t(1, ans);
+        return t * 3; 
     }
 };

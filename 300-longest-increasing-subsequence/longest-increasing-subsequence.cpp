74e428e903d623985c338ddbb5760a75760e61be
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-15 22:05:07 +0530

    Time: 70 ms (59.43%) | Memory: 14.4 MB (64.85%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,7 +3,7 @@ public:
     int lengthOfLIS(vector<int>& arr) {
         int n=arr.size();
         vector<int>dp(n,1);
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int prev=0;prev<i;prev++){
                 if(arr[prev]<arr[i]){
                     dp[i]=max(1+dp[prev],dp[i]);

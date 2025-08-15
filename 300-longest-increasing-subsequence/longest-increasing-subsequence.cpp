c73abee465cb04898444dd26f62b75455e788ed8
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-15 18:21:25 +0530

    Time: 76 ms (53.80%) | Memory: 14.4 MB (64.85%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int solve(int i,int n,int prev,vector<int>& nums,vector<vector<int>>&dp){
-        if(i==n){
-            return 0;
-        }
-        if (dp[i][prev+1]!=-1)return dp[i][prev+1];
-        int nt=0+solve(i+1,n,prev,nums,dp);
-        int t=0;
-        if(prev==-1 || nums[i]>nums[prev]){
-            t=1+solve(i+1,n,i,nums,dp);
-        }
-        return dp[i][prev+1]=max(t,nt);
-
-    }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n+1,0),curr(n+1,0);
-        // return solve(0,n,-1,nums,dp);
-        for(int i=n-1;i>=0;i--){
-            for(int prev=i-1;prev>=-1;prev--){
-                int nt=0+dp[prev+1];
-                int t=0;
-                if(prev==-1 || nums[i]>nums[prev]){
-                    t=1+dp[i+1];
+    int lengthOfLIS(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(arr[prev]<arr[i]){
+                    dp[i]=max(1+dp[prev],dp[i]);
                 }
-                curr[prev+1]=max(t,nt);
             }
-            dp=curr;
         }
-        return dp[0];
+        int ans=1;
+        for(auto x:dp){
+            ans=max(ans,x);
+        }
+        return ans;
     }
 };

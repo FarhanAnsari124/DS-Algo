486acf961575f739d8fa64cef176b0b6208c5ffd
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-19 22:01:52 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (48.06%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,25 +12,32 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        queue<TreeNode*>q;
         vector<vector<int>>ans;
-        if(!root)return{};
-        q.push(root);
-        int size=q.size();
-        bool l_to_r=true;
-        while(!q.empty()){
-            size=q.size();
-            vector<int>v(size);
-            for(int i=0;i<size;i++){
-                TreeNode* temp=q.front();q.pop();
-                if(temp->left)q.push(temp->left);
-                if(temp->right)q.push(temp->right);
-                int ind=(l_to_r)?i:(size-i-1);
-                v[ind]=temp->val;
+        stack<TreeNode*>s1;
+        stack<TreeNode*>s2;
+        if(root==NULL)return ans;
+        s1.push(root);
+        while(!s1.empty() || !s2.empty()){
+            vector<int>v;
+            if(!s1.empty()){
+                while(!s1.empty()){
+                    TreeNode* temp=s1.top();s1.pop();
+                    if(temp->left)s2.push(temp->left);
+                    if(temp->right)s2.push(temp->right);
+                    v.push_back(temp->val);
+                }
+            }else{
+                while(!s2.empty()){
+                    TreeNode* temp=s2.top();s2.pop();
+                    if(temp->right)s1.push(temp->right);
+                    if(temp->left)s1.push(temp->left);
+                    v.push_back(temp->val);
+                }
+
             }
-            l_to_r=!l_to_r;
             ans.push_back(v);
         }
         return ans;
+        
     }
 };

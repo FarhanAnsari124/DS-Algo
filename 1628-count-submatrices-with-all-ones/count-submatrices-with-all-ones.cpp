587a4f8cfb8f91e51650113d6f330e54c86ecc9c
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-22 00:20:45 +0530

    Time: 41 ms (19.75%) | Memory: 19.1 MB (64.27%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int numSubmat(vector<vector<int>>& mat) {
-        vector<vector<int>>ans;
-        int m=mat.size();
-        int n=mat[0].size();
-        for(int sr=0;sr<m;sr++){
-            for(int er=sr;er<m;er++){
-                vector<int>temp;
-                for(int j=0;j<n;j++){
-                    int t=1;
-                    for(int i=sr;i<=er;i++){
-                        t=t&mat[i][j];
-                    }
-                    temp.push_back(t);
-                }
-                ans.push_back(temp);
-            }
-        }
+    int ArrayCount(vector<int>&temp){
         int main=0;
-        for(int i=0;i<ans.size();i++){
-            int cnt=0;
-            for(int j=0;j<ans[0].size();j++){
-                if(ans[i][j]==1){
+        int cnt=0;
+            for(int j=0;j<temp.size();j++){
+                if(temp[j]==1){
                     cnt++;
                 }else{
                     main+=(cnt*(cnt+1))/2;
                     cnt=0;
                 }
             }
-            if(cnt!=0)main+=(cnt*(cnt+1))/2;
-        }
+        if(cnt!=0)main+=(cnt*(cnt+1))/2;
         return main;
+    }
+    int numSubmat(vector<vector<int>>& mat) {
+        vector<vector<int>>ans;
+        int m=mat.size();
+        int n=mat[0].size();
+        int result=0;
+        for(int sr=0;sr<m;sr++){
+            vector<int>temp(n,1);
+            for(int er=sr;er<m;er++){
+                for(int j=0;j<n;j++){
+                    temp[j]=temp[j]&mat[er][j];
+                }
+                result+=ArrayCount(temp);
+            }
+        }
+        return result;
+        
     }
 };

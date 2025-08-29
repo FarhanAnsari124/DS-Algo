3413527c4f7c688a2b52d249459d45e8ecdd6beb
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-08-29 14:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.89%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        long long n_odd=ceil((double)n/2);
-        long long m_even=floor((double)m/2);
-        long long ans=n_odd*m_even;
-        long long n_even=floor((double)n/2);
-        long long m_odd=ceil((double)m/2);
-        ans+=n_even*m_odd;
-        return ans;
+        return (ceil((double)n/2)*floor((double)m/2))+(floor((double)n/2)*ceil((double)m/2));
     }
 };

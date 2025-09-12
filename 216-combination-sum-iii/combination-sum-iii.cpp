8948288867951b805f12208276da84a01a20a8c8
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-09-12 09:49:40 +0530

    Time: 5 ms (7.95%) | Memory: 14.4 MB (9.19%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void solve(int i,int &sum,int target,int k,int size,vector<int>empty){
-        if(empty.size()==size && sum==target){
+    void solve(int i,int target,int k,int size,vector<int>empty){
+        if(empty.size()==size && target==0){
             ans.push_back(empty);
             return;
         }
         if(!k || i>9)return;
-        if(sum+i<=target){
+        if(target-i>=0){
             empty.push_back(i);
-            sum+=i;
-            solve(i+1,sum,target,k-1,size,empty);
-            sum-=i;
+            solve(i+1,target-i,k-1,size,empty);
             empty.pop_back();
         }
-        solve(i+1,sum,target,k,size,empty);
+        solve(i+1,target,k,size,empty);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int>empty;
-        int sum=0;
-        solve(1,sum,n,k,k,empty);
+        solve(1,n,k,k,empty);
         return ans;
         
     }

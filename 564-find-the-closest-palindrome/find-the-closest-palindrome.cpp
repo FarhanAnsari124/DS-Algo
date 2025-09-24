165c0105148dd92ea98800abe60c659e80ded51b
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-09-24 21:06:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (10.52%) - LeetSync

--- 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string get_palindrome(string s,bool even){
+
         if(even){
             string s1=s;
             reverse(s.begin(),s.end());
@@ -11,16 +12,17 @@ public:
             reverse(s.begin(),s.end());
             return s1+s;
         }
+
     }
     string nearestPalindromic(string n) {
+
         int size=n.size();
         bool even=size%2==0;
         vector<long long>v;
         string subst=n.substr(0,ceil((double)n.size()/2));
+
         string c1=get_palindrome(subst,even);
-        if(c1!=n){
-            v.push_back(stoll(c1));
-        }
+        if(c1!=n)v.push_back(stoll(c1));
         subst=to_string(stoll(subst)+1);
         string c2=get_palindrome(subst,even);
         v.push_back(stoll(c2));
@@ -31,9 +33,11 @@ public:
         v.push_back(stoll(c4));
         string c5=to_string((long long)pow(10,size-1)-1);
         v.push_back(stoll(c5));
+
         long long mini=1e15;
         int ansind=-1;
         sort(v.begin(),v.end());
+
         for(int i=0;i<v.size();i++){
             long long diff=abs(v[i]-stoll(n));
             if(diff<mini){
@@ -41,6 +45,7 @@ public:
                 ansind=i;
             }
         }
+
         return to_string(v[ansind]);
         
     }

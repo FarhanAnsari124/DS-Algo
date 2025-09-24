91cc09160f4d5cd6d85501d1940cedefb897b6b7
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-09-24 21:43:35 +0530

    Time: 247 ms (63.25%) | Memory: 10.3 MB (94.10%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<pair<int,int>>dir={{0,-1},{0,1},{-1,0},{1,0}};
-    bool solve(int i,int j,vector<vector<char>>&mat,string s,int k){
+    bool solve(int i,int j,vector<vector<char>>&mat,string &s,int k){
         if(k==s.size())return true;
         if(i<0||i>=mat.size()||j<0||j>=mat[0].size()){
             return false;
@@ -17,9 +17,6 @@ public:
         return orr;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        if(board.size()==1 && board[0].size()==1 && word.size()==1){
-            return word[0]==board[0][0];
-        }
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if(board[i][j]==word[0]){

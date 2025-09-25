2d76a46492dea8de550603007b01b647c2c0efc1
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-09-25 23:33:48 +0530

    Time: 3 ms (13.39%) | Memory: 13.3 MB (42.12%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,39 +1,19 @@
-class Solution
-{
-    public:
-        int solve(int i, int j, int n, vector<vector < int>> &triangle, vector< vector< int>> &dp)
-        {
-            if (i == n - 1)
-            {
-                return triangle[i][j];
-            }
-            if (dp[i][j] != -1e9) return dp[i][j];
-            int down = triangle[i][j] + solve(i + 1, j, n, triangle, dp);
-            int diag = triangle[i][j] + solve(i + 1, j + 1, n, triangle, dp);
-
-            return dp[i][j] = min(down, diag);
+class Solution {
+public:
+    int solve(int i,int n,vector<vector<int>>& triangle,int back,vector<vector<int>>&dp){
+        if(i==n){
+            return 0;
         }
-    int minimumTotal(vector<vector < int>> &triangle)
-    {
-        int n = triangle.size();
-        vector<vector < int>> dp(n, vector<int> (n, -1e9));
-       	// return solve(0,0,n,triangle,dp);
-        vector<int>temp(n,0);
-        for (int j = 0; j < n; j++)
-        {
-            temp[j] = triangle[n - 1][j];
+        if(dp[i][back]!=-1e9)return dp[i][back];
+        int mini=1e9;
+        for(int j=0;j<triangle[i].size();j++){
+           if(j==back || j==back+1)mini=min(mini,triangle[i][j]+solve(i+1,n,triangle,j,dp));
         }
-        for (int i = n - 2; i >= 0; i--)
-        {
-            vector<int>curr(n,0);
-            for (int j = i; j >= 0; j--)
-            {
-                int down = triangle[i][j] + temp[j];
-                int diag = triangle[i][j] + temp[j + 1];
-                curr[j] = min(down, diag);
-            }
-            temp=curr;
-        }
-        return temp[0];
+        return dp[i][back]=mini;
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1e9));
+        return solve(0,n,triangle,0,dp);
     }
 };

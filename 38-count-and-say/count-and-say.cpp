cbfdc549cd35f90dbaf25d1817030ad4b2667c76
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-09-28 16:14:19 +0530

    Time: 3 ms (83.80%) | Memory: 10 MB (46.61%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    string rle(string &s){
+    string make_rle(string s){
+        int i=0;
+        string ans;
+        char curr_char;
         int n=s.size();
-        string ans="";
-        int i=1;
-        int cnt=1;
-        if(n==1){
-            ans+="1";
-            ans+=s[0];
-            return ans;
-        }
         while(i<n){
-            if(s[i]==s[i-1]){
-                cnt+=1;
-            }else{
-                ans+=to_string(cnt);
-                ans+=s[i-1];
-                cnt=1;
+            curr_char=s[i];
+            int cnt=0;
+            while(i<n && s[i]==curr_char){
+                cnt++;
+                i++;
             }
-            i++;
+            ans+=('0'+cnt);
+            ans+=curr_char;
         }
-        ans+=to_string(cnt);
-        ans+=s[n-1];
+        // cout<<s<<':'<<ans<<endl;
         return ans;
     }
     string countAndSay(int n) {
-        vector<string>dp(n+1);
-        dp[1]="1";
-        // cout<<dp[1];
-        for(int i=2;i<=n;i++){
-            string s=dp[i-1];
-            dp[i]=rle(s);
+        vector<string>rle(n+1);
+        rle[1]="1";
+        if(n==1){
+            return rle[n];
         }
-        return dp[n];
+        for(int i=2;i<=n;i++){
+            rle[i]=make_rle(rle[i-1]);
+        }
+        return rle[n];
+        
     }
 };

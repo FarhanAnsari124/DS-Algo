e5cb752c82a7bed09f54776652a8941b8d95556e
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-10-10 16:29:29 +0530

    Time: 877 ms (16.25%) | Memory: 112.3 MB (34.25%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -9,29 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+struct info{
+    int sum;
+    int min;
+    int max;
+    bool isbst;
+}; 
 class Solution {
 public:
     int maxsum=0;
-    vector<int>solve(TreeNode* root){
+    info solve(TreeNode* root){
         if(root==nullptr){
             return {0,(int)1e5,(int)-1e5,1};//{sum,min,max,isbst}
         }
-        vector<int>left=solve(root->left);
-        vector<int>right=solve(root->right);
-        bool l=left[3];
-        bool r=right[3];
-        if(r && l && root->val>left[2] && root->val<right[1]){
-            int sum=left[0]+right[0]+root->val;
+        info left=solve(root->left);
+        info right=solve(root->right);
+        bool l=left.isbst;
+        bool r=right.isbst;
+        if(r && l && root->val>left.max && root->val<right.min){
+            int sum=left.sum+right.sum+root->val;
             maxsum=max(sum,maxsum);
-            int mini=min({left[1],root->val});
-            int maxi=max({right[2],root->val});
+            int mini=min({left.min,root->val});
+            int maxi=max({right.max,root->val});
             return {sum,mini,maxi,1};
         }else{
             return {0,0,0,0};
         }
     }
     int maxSumBST(TreeNode* root) {
-        vector<int>temp=solve(root);
+        info temp=solve(root);
         return maxsum;
     }
 };

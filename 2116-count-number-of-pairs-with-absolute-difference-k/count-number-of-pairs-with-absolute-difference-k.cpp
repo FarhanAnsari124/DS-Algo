acf358e7acb74f5259009a085024ea10a21a884c
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-10-15 00:36:18 +0530

    Time: 6 ms (22.25%) | Memory: 17.1 MB (18.42%) - LeetSync

---
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& arr, int target) {
+        unordered_map<int,int>mapy;
+        int n=arr.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int r1=target+arr[i];
+            int r2=arr[i]-target;
+            if(mapy.find(r1)!=mapy.end()){
+                cnt+=mapy[r1];
+            }
+            if(mapy.find(r2)!=mapy.end()){
+                cnt+=mapy[r2];
+            }
+            mapy[arr[i]]++;
+        }
+        return cnt;
+    }
+};

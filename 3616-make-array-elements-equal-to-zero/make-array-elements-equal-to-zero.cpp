eb6995b1902236c509af30da2a14d86d7e3fa019
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-10-28 22:21:53 +0530

    Time: 188 ms (10.55%) | Memory: 24.1 MB (18.81%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
+    int count(int i,bool front,vector<int>&nums){
+        vector<int>copyx(nums);
+        int n=nums.size();
+        int curr=i;
+        while(curr>=0 && curr<n){
+            if(copyx[curr]>0){
+                copyx[curr]--;
+                front=!front;
+            }
+            if(front){
+                curr++;
+            }else{
+                curr--;
+            }
+        }
+        int sum=0;
+        for(auto x:copyx){
+            sum+=x;
+        }
+        if(!sum)return 1;
+        return 0;
+    }
     int countValidSelections(vector<int>& nums) {
         int cnt=0;
         int n=nums.size();
       for(int i=0;i<n;i++){
         if(nums[i]==0){
-            vector<int>copyx(nums);
-            int curr=i;
-            bool front=true;
-            while(curr>=0 && curr<n){
-                if(copyx[curr]>0){
-                    copyx[curr]--;
-                    front=!front;
-                }
-                if(front){
-                    curr++;
-                }else{
-                    curr--;
-                }
-            }
-            int sum=0;
-            for(auto x:copyx){
-                sum+=x;
-            }
-            if(!sum)cnt++;
-            copyx=nums;
-            curr=i;
-            front=false;
-            while(curr>=0 && curr<n){
-                if(copyx[curr]>0){
-                    copyx[curr]--;
-                    front=!front;
-                }
-                if(front){
-                    curr++;
-                }else{
-                    curr--;
-                }
-            }
-            sum=0;
-            for(auto x:copyx){
-                sum+=x;
-            }
-            if(!sum)cnt++;
+            cnt+=(count(i,true,nums)+count(i,false,nums));
         }
       }  
       return cnt;

974252f254eb88f86e04a0420518e91a1de94ce5
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-11-24 22:23:40 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (32.08%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        unordered_map<int,int>mapy;
+        map<int,int>mapy;
         int n=nums.size();
         for(int i=0;i<n;i++){
             mapy[nums[i]]=i;

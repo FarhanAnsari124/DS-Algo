bdf5a0f1ed76f518c4b12a32150e161ee4a7c240
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-11-24 22:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.08%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        map<int,int>mapy;
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            mapy[nums[i]]=i;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target==nums[mid])return mid;
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }
+                else low=mid+1;
+            }else if(nums[mid]<nums[high]){
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }
+                else high=mid-1;
+            }
         }
-        if(mapy.find(target)!=mapy.end())
-        return mapy[target];
-        else return -1;
+        return -1;
     }
 };

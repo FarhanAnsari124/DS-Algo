7ce052c69cbc0a0311b3ee61088c953d591c23b7
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-12-23 23:43:54 +0530

    Time: 35 ms (88.17%) | Memory: 45.4 MB (55.41%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,15 +4,15 @@ public:
         unordered_map<int,int>mapy;
         int n=arr.size();
         int cnt=0;
-        vector<int>psum(n+1,0);
+        int sum=0;
         mapy[0]=1;
-        for(int i=1;i<=n;i++)psum[i]=psum[i-1]+arr[i-1];
-        for(int i=1;i<=n;i++){
-            int rem=psum[i]-k;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            int rem=sum-k;
             if(mapy.find(rem)!=mapy.end()){
                 cnt+=mapy[rem];
             }
-            mapy[psum[i]]++;
+            mapy[sum]++;
         }
         return cnt;
     }

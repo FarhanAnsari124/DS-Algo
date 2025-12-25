e97129eb1bcc6057c805d77fa68fa44ddb47fc75
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-12-25 19:31:45 +0530

    Time: 151 ms (75.18%) | Memory: 126.3 MB (5.67%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& v, int k) {
+        priority_queue<long long>pq;
+        for(auto x:v){
+            pq.push(x);
+        }
+        long long i=0;
+        long long sum=0;
+        while(k--){
+            if(pq.top()>i)sum+=(pq.top()-i);
+            else break;
+            pq.pop();
+            i++;
+        }
+        return sum;
+
+    }
+};

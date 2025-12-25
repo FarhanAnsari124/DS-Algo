2e6ec42abc4cb5e929471364a39fa0a6865e937c
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-12-25 19:32:16 +0530

    Time: 160 ms (49.65%) | Memory: 126.4 MB (5.67%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -8,7 +8,7 @@ public:
         long long i=0;
         long long sum=0;
         while(k--){
-            if(pq.top()>i)sum+=(pq.top()-i);
+            if(pq.top()>=i)sum+=(pq.top()-i);
             else break;
             pq.pop();
             i++;

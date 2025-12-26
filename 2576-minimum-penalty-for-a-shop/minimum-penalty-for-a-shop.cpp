498806f6a33737a71a8da47a82d22a7b54a911d8
FarhanAnsari124 <Farhan.knp121@gmail.com>
2025-12-26 13:25:56 +0530

    Time: 7 ms (67.76%) | Memory: 13.1 MB (98.36%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bestClosingTime(string c) {
+       int s=0;
+       int n=c.size();
+       for(auto x:c){
+        if(x=='Y')s++;
+       }
+       int ans=s;
+       int ind=0;
+       for(int i=1;i<=n;i++){
+            if(c[i-1]=='Y'){
+                s--;
+                if(s<ans){
+                    ans=s;
+                    ind=i;
+                }
+            }else{
+                s++;
+                if(s<ans){
+                    ans=s;
+                    ind=i;
+                }
+            }
+       }
+       return ind; 
+    }
+};

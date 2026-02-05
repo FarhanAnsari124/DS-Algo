278b5dcbaf3c4e8c7bfd2f664b624e86ec2eb863
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-02-05 16:18:02 +0530

    Time: 15 ms (94.33%) | Memory: 12.7 MB (80.88%) - LeetSync

---
+++ 843-binary-trees-with-factors/binary-trees-with-factors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int MOD=1e9+7;
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        unordered_map<long long,long long>mapy;
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        for(auto x:arr){
+            mapy[x]=1;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]%arr[j]==0 && mapy.find((long long)arr[i]/arr[j])!=mapy.end()){
+                    mapy[arr[i]]+=(mapy[arr[j]]%MOD)*(mapy[(long)arr[i]/arr[j]]%MOD)%MOD;
+                }
+            }
+        }
+        long long cnt=0;
+        for(auto x:mapy){
+            cnt+=x.second;
+        }
+        return cnt%MOD;
+    }
+};

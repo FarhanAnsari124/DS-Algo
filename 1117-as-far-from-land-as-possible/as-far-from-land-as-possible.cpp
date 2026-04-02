f79a346134f90d8fb3ff2a278d69f3a61ccf9331
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-04-02 10:43:46 +0530

    Time: 13 ms (77.32%) | Memory: 25.3 MB (59.06%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<pair<int,int>>dir={{-1,0},{1,0},{0,1},{0,-1}};
-    int maxDistance(vector<vector<int>>& g) {
-        int n=g.size();
-        int m=g[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<int,int>>q;
+    int maxDistance(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && g[i][j]==1){
-                    q.push({i,j});
+                if(grid[i][j]==1){
+                    q.push({{i,j},0});
                 }
             }
         }
         int maxi=-1;
-        vector<vector<int>>dis(n,vector<int>(m,0));
         while(!q.empty()){
-            auto y=q.front();q.pop();
+            auto top=q.front();q.pop();
+            int cr=top.first.first;
+            int cc=top.first.second;
+            int dist=top.second;
             for(auto x:dir){
-                int sr=x.first+y.first;
-                int sc=x.second+y.second;
-                if(sr>=0 && sr<n && sc>=0 && sc<m){
-                    int len=0;
-                    if(!vis[sr][sc] && g[sr][sc]==0){
-                        vis[sr][sc]=1;
-                        q.push({sr,sc});
-                        dis[sr][sc]=dis[y.first][y.second]+1;
-                        maxi=max(maxi,dis[sr][sc]);
-                    }
+                int nr=x.first+cr;
+                int nc=x.second+cc;
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==0){
+                    grid[nr][nc]=1;
+                    int nd=dist+1;
+                    maxi=max(nd,maxi);
+                    q.push({{nr,nc},nd});
                 }
             }
+
         }
         return maxi;
+        
+
     }
 };

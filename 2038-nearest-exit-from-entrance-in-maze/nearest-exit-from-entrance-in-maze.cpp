5e16142e4d56ad9472e1768fc32ea99daa623d65
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-04-03 11:46:22 +0530

    Time: 0 ms (100.00%) | Memory: 35.5 MB (62.30%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -5,15 +5,6 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         queue<pair<pair<int,int>,int>>q;
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         if(grid[i][j]==1){
-        //             q.push({{i,j},0});
-        //         }
-        //     }
-        // }
-        // int mini=-1;
-        // q.push({{e[0],e[1]},0});
         grid[e[0]][e[1]]='+';
         for(auto x:dir){
             int nr=e[0]+x.first;
@@ -34,7 +25,6 @@ public:
                 if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='.'){
                     grid[nr][nc]='+';
                     int nd=dist+1;
-                    // maxi=max(nd,maxi);
                     q.push({{nr,nc},nd});
                 }else if(nr<0 || nr>=n || nc<0 || nc>=m){
                     return dist;

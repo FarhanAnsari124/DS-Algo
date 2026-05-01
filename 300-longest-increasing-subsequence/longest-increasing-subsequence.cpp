2f4f49fd40f43a809100e1f87291a88391b0930a
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-05-01 17:52:40 +0530

    Time: 56 ms (61.47%) | Memory: 14.3 MB (67.20%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int lis(int i,int pre,vector<int>&nums,vector<vector<int>>&dp){
-        if(i>=nums.size()){
-            return 0;
-        }
-        if(dp[i][pre+1]!=-1)return dp[i][pre+1];
-        int nt,t=0;
-        nt=0+lis(i+1,pre,nums,dp);
-        if(pre==-1 || nums[i]>nums[pre]){
-            t=1+lis(i+1,i,nums,dp);
-        }
-        return dp[i][pre+1]=max(t,nt);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(nums.size(),1);
-        for(int i=1;i<n;i++){
-            for(int pre=0;pre<i;pre++){
-                if(nums[i]>nums[pre]){
-                    dp[i]=max(dp[i],dp[pre]+1);
-                }
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j])dp[i]=max(dp[j]+1,dp[i]);
             }
         }
-        int ans=1;
+        int maxi=1;
+        // return dp[n-1];
         for(auto x:dp){
-            ans=max(x,ans);
+            maxi=max(maxi,x);
         }
-        return ans;
+        return maxi;
     }
 };

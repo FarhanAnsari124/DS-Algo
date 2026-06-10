d049a5585bc91ad3e2e7283507663fb323ed212b
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-06-10 14:42:38 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (12.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,11 +3,12 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         int cal_xor=0;
-        for(int i=0;i<=n;i++)cal_xor^=i;
         int ac_xor=0;
-        for(auto x:nums){
-            ac_xor^=x;
+        for(int i=0;i<n;i++){
+            cal_xor^=i;
+            ac_xor^=nums[i];
         }
+        cal_xor^=n;
         return cal_xor^ac_xor;
     }
 };

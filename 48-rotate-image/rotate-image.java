4171ac05c21f1e0c8fa1b474cd453f991ded7ee3
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-06-20 14:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (68.66%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void reverse(int [] arr){
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+                if(i!=j){
+                    int temp=matrix[i][j];
+                    matrix[i][j]=matrix[j][i];
+                    matrix[j][i]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i]);
+        }
+    }
+}

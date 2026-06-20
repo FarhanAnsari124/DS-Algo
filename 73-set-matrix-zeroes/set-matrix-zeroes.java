cfd6f35521ad4b4e1ce00b117fdb1e0cd25e65a7
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-06-20 13:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (61.58%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,21 +2,27 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
-        int [][] copy = new int[n][m];
-        for(int i=0;i<n;i++){ //Copying my original matrix into copy matrix
+        int [] rows = new int[n];
+        int [] cols = new int[m];
+        for(int i=0;i<n;i++){//maintaing two 1D-arrays,marking it as 1 if im getting a zero at i,j;
             for(int j=0;j<m;j++){
-                copy[i][j] = matrix[i][j];
+                if(matrix[i][j]==0){
+                    rows[i]=1;
+                    cols[j]=1;
+                }
             }
         }
-        for(int i=0;i<n;i++){//Traversing on copy array
-            for(int j=0;j<m;j++){
-                if(copy[i][j]==0){//cheching if an element is zero
-                    for(int k=0;k<n;k++){//making the 'J' coloumn as zero
-                        matrix[k][j]=0;
-                    }
-                    for(int k=0;k<m;k++){//making the 'I' row as zero
-                        matrix[i][k]=0;
-                    }
+        for(int i=0;i<n;i++){//making the whole row as zero if im getting 1 in rows[i];
+            if(rows[i]==1){
+                for(int k=0;k<m;k++){
+                    matrix[i][k]=0;
+                }
+            }
+        }
+        for(int j=0;j<m;j++){//making the whole coloumn zero if im getting 1 in cols[j];
+            if(cols[j]==1){
+                for(int k=0;k<n;k++){
+                    matrix[k][j]=0;
                 }
             }
         }

4c0e1f41a6a2c7fe33a4cfc9a2d08a9df8aec5c9
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-07-01 09:57:09 +0530

    Time: 3 ms (87.51%) | Memory: 42.9 MB (92.98%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st = new Stack<>();
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty())return false;
+                char top = st.pop();
+                if((top=='(' && ch==')') || (top=='{' && ch=='}') || (top=='[' && ch==']')){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(st.isEmpty())return true;
+        return false;
+    }
+}

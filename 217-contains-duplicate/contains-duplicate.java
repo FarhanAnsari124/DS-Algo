622fb70751d0ae5adcd95d0531f605826573c0c7
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-07-02 14:37:41 +0530

    Time: 46 ms (5.05%) | Memory: 110.9 MB (5.12%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> freq_map = new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            freq_map.put(nums[i],freq_map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> elem : freq_map.entrySet()){
+            int value = elem.getValue();
+            if(value>1){
+                return true;
+            }
+        }
+        return false;
+    }
+}

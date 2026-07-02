20d364b0f7325c90dde28437a87c07363acc3003
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-07-02 14:38:54 +0530

    Time: 35 ms (5.05%) | Memory: 110.7 MB (5.12%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -5,12 +5,12 @@ class Solution {
         for(int i=0;i<n;i++){
             freq_map.put(nums[i],freq_map.getOrDefault(nums[i],0)+1);
         }
-        for(Map.Entry<Integer,Integer> elem : freq_map.entrySet()){
-            int value = elem.getValue();
-            if(value>1){
-                return true;
-            }
-        }
-        return false;
+        // for(Map.Entry<Integer,Integer> elem : freq_map.entrySet()){
+        //     int value = elem.getValue();
+        //     if(value>1){
+        //         return true;
+        //     }
+        // }
+        return n!=freq_map.size();
     }
 }

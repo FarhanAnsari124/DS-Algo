252dd4a688f42704f8e0e705c29d8f2f8d7c0235
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-07-07 11:18:55 +0530

    Time: 1 ms (100.00%) | Memory: 118.9 MB (72.63%) - LeetSync

--- 4341-maximum-valid-pair-sum/maximum-valid-pair-sum.java
+++ 4341-maximum-valid-pair-sum/maximum-valid-pair-sum.java
@@ -1,15 +1,12 @@
 class Solution {
-    public int maxValidPairSum(int[] arr, int k) {
-        int n=arr.length;
-        int ans=0;
-        int[] suff = new int[n];
-        suff[n-1]=arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            suff[i]=Math.max(suff[i+1],arr[i]);
+    public int maxValidPairSum(int[] nums, int k) {
+        int maxi=-1;
+        int sum=0;
+        int n=nums.length;
+        for(int i=k;i<n;i++){
+            maxi=Math.max(maxi,nums[i-k]);
+            sum=Math.max(sum,nums[i]+maxi);
         }
-        for(int i=0;i<(n-k);i++){
-            ans=Math.max(ans,arr[i]+suff[i+k]);
-        }
-        return ans;
+        return sum;
     }
 }

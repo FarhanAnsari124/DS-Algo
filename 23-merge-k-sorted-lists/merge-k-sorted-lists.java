ccaca174d1c91a1070f3d52eabb0dce0a61e8286
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-07-09 09:49:54 +0530

    Time: 5 ms (41.66%) | Memory: 47 MB (28.91%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,34 +8,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Pair {
+class Pair{
     int first;
     ListNode second;
-
-    Pair(int first, ListNode second) {
-        this.first = first;
-        this.second = second;
+    Pair(int first,ListNode second){
+        this.first=first;
+        this.second=second;
     }
 }
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.first,b.first));
-        for(ListNode x: lists){
-            if(x!=null){
-                pq.add(new Pair(x.val,x));
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>((a,b)-> Integer.compare(a.first,b.first));
+        int n=lists.length;
+        for(int i=0;i<n;i++){
+            if(lists[i]!=null){
+                minHeap.add(new Pair(lists[i].val,lists[i]));
             }
         }
-        ListNode temp=new ListNode(-1);
-        ListNode t=temp;
-        while(!pq.isEmpty()){
-            Pair x =pq.poll();
-            temp.next=x.second;
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while(!minHeap.isEmpty()){
+            Pair p=minHeap.poll();
+            int node_val = p.first;
+            ListNode node = p.second;
+            temp.next = node;
             temp=temp.next;
-            if(x.second.next!=null){
-                pq.add(new Pair(x.second.next.val,x.second.next));
+            if(node.next!=null){
+                minHeap.add(new Pair(node.next.val,node.next));
             }
         }
-        return t.next;
-        
+        return dummy.next;
     }
 }

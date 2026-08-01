4d199abfbc5f3f5f2dd4bfbfe4e4db23f1c8cdcb
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-08-01 10:53:20 +0530

    Time: 110 ms (11.70%) | Memory: 9.7 MB (69.71%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int i,int j,int p1,int p2,bool flag,vector<int>&nums){
+        if(i == j){
+            if(flag)
+                return (p1+nums[i])>=p2;
+            else
+                return p1>=(p2+nums[i]);
+        }
+        if(flag){
+            int fr=solve(i+1,j,p1+nums[i],p2,false,nums);
+            int back=solve(i,j-1,p1+nums[j],p2,false,nums);
+            return fr || back;
+        }else{
+            int fr=solve(i+1,j,p1,p2+nums[i],true,nums);
+            int back=solve(i,j-1,p1,p2+nums[j],true,nums);
+            return fr && back;
+        }
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return true;
+        return solve(0,n-1,0,0,true,nums);
+    }
+};

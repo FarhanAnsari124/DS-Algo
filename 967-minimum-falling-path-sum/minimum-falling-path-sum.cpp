788036820be568770d61c1e6056bdbb4a493409d
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-08-01 14:39:25 +0530

    Time: 6 ms (13.17%) | Memory: 16.3 MB (8.10%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int solve(int i,int j,int m,int n,vector<vector<int>>&mat,vector<vector<int>>&dp){
-        if(i>m || j<0 || j>=n){
+        if(j<0 || j>=n){
             return 1e9;
         }
         if(i==m-1)return mat[i][j];
-        if(dp[i][j]!=-1){
+        if(dp[i][j]!=-101){
             return dp[i][j];
         }
         int ld = mat[i][j]+solve(i+1,j-1,m,n,mat,dp);
@@ -17,20 +17,9 @@ public:
         int mini=1e9;
         int m=mat.size();
         int n=mat[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,1e9));
-        for(int i=0;i<n;i++)dp[m-1][i]=mat[m-1][i];
-        for(int i=m-2;i>=0;i--){
-            for(int j=0;j<n;j++){
-                int ld=1e9;
-                int rd=1e9;
-                if(j!=0)ld = mat[i][j]+dp[i+1][j-1];
-                int d = mat[i][j]+dp[i+1][j];
-                if(j!=n-1)rd = mat[i][j]+dp[i+1][j+1];
-                dp[i][j]=min({ld,d,rd});
-            }
-        }
+        vector<vector<int>>dp(m,vector<int>(n,-101));
         for(int i=0;i<n;i++){
-            mini=min(mini,dp[0][i]);
+            mini=min(solve(0,i,m,n,mat,dp),mini);
         }
         return mini;
     }

4d0132165629a17b5b0087ec9443fa28830fa7d4
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-08-20 20:32:28 +0530

    Time: 968 ms (5.45%) | Memory: 0B (100.00%) - LeetSync

---
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT name AS Customers
+FROM Customers
+WHERE id
+NOT IN(
+    SELECT c.id
+    FROM Customers c
+    JOIN Orders o
+    ON c.id = o.customerId
+)

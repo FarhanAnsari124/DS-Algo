18fe506c80a8be837b6be06c822eaabb6116d956
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-08-22 10:50:28 +0530

    Time: 402 ms (47.66%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -3,6 +3,6 @@ WITH cte AS
 (SELECT query_name,rating/position AS ratio, CASE WHEN rating<3 THEN 1
 ELSE 0  END AS quality_binary
 FROM Queries)
-SELECT query_name,ROUND(SUM(ratio)/COUNT(query_name),2) AS quality,ROUND(SUM(quality_binary)/COUNT(query_name)*100,2) AS poor_query_percentage
+SELECT query_name,ROUND(AVG(ratio),2) AS quality,ROUND(SUM(quality_binary)/COUNT(query_name)*100,2) AS poor_query_percentage
 FROM cte
 GROUP BY query_name

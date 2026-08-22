372ae668478e6459961939627b3a98a4db431952
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-08-22 10:03:03 +0530

    Time: 282 ms (56.56%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,10 +1,14 @@
 # Write your MySQL query statement below
-SELECT *
-FROM Cinema c2
-WHERE c2.id NOT IN
+-- SELECT *
+-- FROM Cinema c2
+-- WHERE c2.id NOT IN
+WITH cte AS
 (
 SELECT c.id
 FROM Cinema c
 WHERE c.id%2=0 OR c.description='boring'
 )
+SELECT *
+FROM Cinema c2
+WHERE c2.id NOT IN (SELECT id FROM cte)
 ORDER BY rating DESC

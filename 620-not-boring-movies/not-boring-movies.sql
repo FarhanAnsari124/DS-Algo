b2d89bc333f494a04baa098654e96b6530b822ec
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-08-22 10:04:30 +0530

    Time: 259 ms (85.66%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -2,13 +2,13 @@
 -- SELECT *
 -- FROM Cinema c2
 -- WHERE c2.id NOT IN
-WITH cte AS
-(
-SELECT c.id
-FROM Cinema c
-WHERE c.id%2=0 OR c.description='boring'
-)
+-- WITH cte AS
+-- (
 SELECT *
-FROM Cinema c2
-WHERE c2.id NOT IN (SELECT id FROM cte)
+FROM Cinema c
+WHERE c.id%2<>0 AND c.description<>'boring'
+-- )
+-- SELECT *
+-- FROM Cinema c2
+-- WHERE c2.id NOT IN (SELECT id FROM cte)
 ORDER BY rating DESC

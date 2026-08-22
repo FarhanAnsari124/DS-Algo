10c95c05d8de1f87f00f32cf2204640d1db22825
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-08-22 10:44:50 +0530

    Time: 370 ms (74.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+WITH cte AS
+(SELECT query_name,rating/position AS ratio, CASE WHEN rating<3 THEN 1
+ELSE 0  END AS quality_binary
+FROM Queries)
+SELECT query_name,ROUND(SUM(ratio)/COUNT(query_name),2) AS quality,ROUND(SUM(quality_binary)/COUNT(query_name)*100,2) AS poor_query_percentage
+FROM cte
+GROUP BY query_name

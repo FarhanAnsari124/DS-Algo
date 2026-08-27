8ff98c433005f11055217602636c0c29e2fb06d0
FarhanAnsari124 <Farhan.knp121@gmail.com>
2026-08-27 10:13:04 +0530

    Time: 306 ms (20.54%) | Memory: 20.6 MB (24.38%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int i,int j,string &s,int q,int n){
+        if(j>=n || i<0 || j<0 || i>=n || i>j){
+            return 0;
+        } 
+        unordered_map<char,int>mapy;
+        for(int k=i;k<=j;k++){
+            mapy[s[k]]++;
+        }
+        int maxi=0;
+        for(int k=i;k<=j;k++){
+            if(mapy[s[k]]<q){
+                return maxi=max(solve(i,k-1,s,q,n),solve(k+1,j,s,q,n));
+            }
+        }
+        return j-i+1;
+    }
+    int longestSubstring(string s, int k) {
+        int n=s.size();
+        return solve(0,s.size()-1,s,k,n);
+    }
+};
